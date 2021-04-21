f329bfe1ce423703dccab2778edb7f328cf0c954
mikejguo290 <mikejguo290@gmail.com>
2021-04-21 17:22:16 +0100

    implemented Lodash inRange() method.

--- _.js
+++ _.js
@@ -1,6 +1,7 @@
 const _={
     clamp(number, lower, upper){
-        /* clamp takes in three args. a number, an upper bound and lower bound, 
+        /* 
+        clamp takes in three args. a number, an upper bound and lower bound, 
         if the number is smaller than the lower bound, return the lower bound.
         if the number is larger than the upper bound, return the upper bound.
         else, return the provided number as is. 
@@ -10,6 +11,37 @@ const _={
         //arg > upperBound? Math.min(arg, upperBound) : Math.max(arg, lowerBound);
          
         return clampedValue
+    },
+    inRange(number, start, end){
+        /* 
+        inRange checks if number is in range between start and up to but including end.
+        returns true if within range and false is not within range. 
+        if only two arguments are provided, start is set to 0, 
+        if start > end, reverse start and end to support negative ranges. 
+        */ 
+       /* my gameplan.
+        if (start>end){
+            const startRange=end;
+            const endRange=start;
+        }else{
+            const startRange=start;
+            const endRange=end;
+        };
+        // check if clamp() of the number is the same as the number
+        return number=this.clamp(number, startRange, endRange-1)
+       */
+
+        if (end===undefined){
+            end=start;
+            start=0;
+        }
+        if (start>end){
+            let temp=end;
+            end=start;
+            start=temp;
+        }
+        const isInRange = number >= start && number < end ; 
+        return isInRange;
     },
 };
 
