5508bb3b22c9a534beb88898d0daf2591430d424
mikejguo290 <mikejguo290@gmail.com>
2021-04-21 16:39:06 +0100

    clamp method with 3 arguments, input number, upper bound and lower bound.

--- _.js
+++ _.js
@@ -1,4 +1,17 @@
-
+const _={
+    clamp(number, lower, upper){
+        /* clamp takes in three args. a number, an upper bound and lower bound, 
+        if the number is smaller than the lower bound, return the lower bound.
+        if the number is larger than the upper bound, return the upper bound.
+        else, return the provided number as is. 
+        */
+        const lowerClampedValue=Math.max(number, lower);
+        const clampedValue=Math.min(lowerClampedValue, upper)
+        //arg > upperBound? Math.min(arg, upperBound) : Math.max(arg, lowerBound);
+         
+        return clampedValue
+    },
+};
 
 
 
